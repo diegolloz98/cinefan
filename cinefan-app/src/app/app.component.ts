2b11628f6be42a05308bafed8d6e5a15fc6c7620
The Outl4nder <theoutl4nder@gmail.com>
2021-04-24 13:46:52 -0500

    google front end auth works

--- cinefan-app/src/app/app.component.ts
+++ cinefan-app/src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
-import { GoogleSigninService } from './google-signin.service';
+import {SocialAuthService, GoogleLoginProvider} from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'cinefan-app';
 
-  constructor(private signInServices: GoogleSigninService, private ref: ChangeDetectorRef){
+  constructor(private authService:SocialAuthService){
+    
+  }
 
+  ngOnInit():void{}
+
+  googleLogin(){
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
 }
--- cinefan-app/src/app/google-signin.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class GoogleSigninService {
-
-}
