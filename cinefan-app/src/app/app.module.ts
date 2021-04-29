ef02b46b6148113e706757db0ff717b0d61c54ee
diegolloz98 <72325707+diegolloz98@users.noreply.github.com>
2021-04-28 23:36:33 -0500

    vista
    dd

--- cinefan-app/src/app/app.module.ts
+++ cinefan-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment} from './../environments/environment';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes=[];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
