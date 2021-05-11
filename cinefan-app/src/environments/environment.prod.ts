bdf7d2db41f4ba7d767afb51112c533dc8b30165
The Outl4nder <theoutl4nder@gmail.com>
2021-05-10 21:35:48 -0500

    1.0 PLZ

--- cinefan-app/src/environments/environment.prod.ts
+++ cinefan-app/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   client_id: '164284753373-cmqnrntrfvevtnutmoj53ftinep3u37c.apps.googleusercontent.com',
   client_secret: 'It9DnLFtLY3mf6duQAUnNvG0',
-  socketUrl: 'https://cinefan.herokuapp.com/api/'
+  socketUrl: 'https://cinefan-app.herokuapp.com/api/'
 };
